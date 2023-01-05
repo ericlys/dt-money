9cde61c6ce2188e911dda9017ab4572e93bd2ab2
Ericlys Moreira <ericlysm@gmail.com>
2023-01-05 20:41:18 -0300

    chore: newtransaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import { HeaderContaiener, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from '../../assets/logo.svg';
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
 
 export function Header() {
   return (
@@ -8,7 +11,14 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+        
       </HeaderContent>
     </HeaderContaiener>
   )
