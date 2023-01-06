bd8f147189daa6582b00651786ede9a8819ad160
Ericlys Moreira <ericlysm@gmail.com>
2023-01-06 13:46:17 -0300

    chore: calculating summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,10 +6,28 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
   const theme = useTheme();
+  const { transactions } = useContext(TransactionsContext);
 
-  const { transactions } = useContext(TransactionsContext)
 
-  console.log(transactions)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income'){
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0, 
+      total: 0
+    } 
+    )
+
 
   return (
     <SummaryContainer>
@@ -19,7 +37,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={theme["green-300"]}/>
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -28,7 +46,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={theme["red-300"]}/>
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -37,7 +55,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={theme["white"]}/>
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
