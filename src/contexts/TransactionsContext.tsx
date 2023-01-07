0e9a21545722058e7cd88b262b818faf3852d7e4
Ericlys Moreira <ericlysm@gmail.com>
2023-01-07 10:32:54 -0300

    chore: creating new transaction

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,17 @@ interface Transactions {
   createdAt: string;
 }
 
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
+
 interface TransactionContextType {
   transactions: Transactions[];
   fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 
 // const TransactionContext = createContext<TransactionContextType>({
@@ -32,11 +40,27 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   async function fetchTransactions(query?: string) {
     const response = await api.get('transactions', {
       params: {
+        _sort: 'createdAt',
+        _order: 'desc',
         q: query,
       }
     });
 
     setTransactions(response.data);
+  }
+
+  async function createTransaction(data: CreateTransactionInput) {
+    const {description, price, category, type} = data;
+
+    const response = await api.post('/transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions(state => [response.data, ...state])
   }
 
   useEffect(() => {
@@ -47,6 +71,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     <TransactionsContext.Provider value={{ 
       transactions, 
       fetchTransactions,
+      createTransaction
     }}>
       {children}
     </TransactionsContext.Provider>
